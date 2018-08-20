894d0c26f3f0841749a78204bf4ac553aba79c66
xieyangxuejun <xieyangxuejun@gmail.com>
2018-08-20 12:49:38 +0800

    update

--- emoji/src/main/java/com/foretree/support/emoji/EmojiUtils.kt
+++ emoji/src/main/java/com/foretree/support/emoji/EmojiUtils.kt
@@ -17,35 +17,16 @@ object EmojiUtils {
 
     @JvmStatic
     fun replaceEmoji(tv: TextView): Spannable {
-        val content = tv.text.toString()
-
-        val emojiEntities = arrayListOf<RegEntity>()
-        Pattern.compile(regx).matcher(content).run {
-            while (this.find()) {
-                emojiEntities.add(RegEntity(this.start(), this.end(), this.group()))
-            }
-        }
-
-        val ssb = SpannableStringBuilder(tv.text)
-        for (entity in emojiEntities) {
-            val emojiEntity = EmojiManager.getInstance().getEmojiEntity(entity.value) ?: continue
-            val drawable = EmojiManager.getInstance().getEmojiDrawable(tv.resources, emojiEntity)
-                    ?: continue
-            val textSize = tv.textSize.toInt()
-            drawable.setBounds(0, 0, textSize, textSize)
-            val imageSpan = ImageSpan(drawable, DynamicDrawableSpan.ALIGN_BOTTOM)
-            val ss = SpannableString(entity.value)
-            ss.setSpan(imageSpan, 0, ss.length, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE)
-
-            //插入需要将以前的字符移除
-            ssb.delete(entity.start, entity.end)
-            ssb.insert(entity.start, ss)
-        }
-        return ssb
+        return replaceEmoji(tv.resources, tv.text, tv.textSize.toInt())
     }
 
     @JvmStatic
-    fun replaceEmoji(resources: Resources, content: CharSequence): Spannable {
+    fun replaceEmoji(resources: Resources, content: CharSequence, textSize: Int): Spannable {
+        return replaceEmoji(resources, content, textSize, textSize)
+    }
+
+    @JvmStatic
+    fun replaceEmoji(resources: Resources, content: CharSequence, width: Int, height: Int): Spannable {
         val emojiEntities = arrayListOf<RegEntity>()
         Pattern.compile(regx).matcher(content).run {
             while (this.find()) {
